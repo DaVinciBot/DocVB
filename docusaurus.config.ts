9742f5074c5ad648da1a7beb6505fa5709d6de82
austin <eliottroussille@gmail.com>
2026-09-06 18:27:38 +0200

    feat: enable faster future flag and update markdown link handling

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    faster: true,
   },
 
   // Set the production url of your site here
@@ -29,10 +30,10 @@ const config: Config = {
   projectName: "DocVB", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
   markdown: {
     mermaid: true,
+    hooks: { onBrokenMarkdownLinks: "warn" },
   },
 
   // Even if you don't use internationalization, you can use this field to set
